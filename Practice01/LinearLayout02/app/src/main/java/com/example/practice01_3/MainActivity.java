b9b5bed13f0086f0b086342f786f72e58fe8852a
Alejandro Zepeda <alexzepeda07@hotmail.com>
2021-01-21 08:15:02 -0600

    Práctica 01 - Plantillas

---
+++ Practice01/LinearLayout02/app/src/main/java/com/example/practice01_3/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.practice01_3;
+
+import android.os.Bundle;
+import android.app.Activity;
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_main);
+    }
+}
