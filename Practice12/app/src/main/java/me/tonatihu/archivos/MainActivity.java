7c82e5547e70c6ba92db6b5add985478b8e9ee77
Alejandro Zepeda <alexzepeda07@hotmail.com>
2021-01-29 20:28:27 -0600

    Práctica 12 - Activity

---
+++ Practice12/app/src/main/java/me/tonatihu/archivos/MainActivity.java
@@ -0,0 +1,23 @@
+package me.tonatihu.archivos;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickUno(View view) {
+        startActivity(new Intent(this, ArchivosActivity.class));
+    }
+
+    public void onClickDos(View view) {
+        startActivity(new Intent(this, ArchivosDosActivity.class));
+    }
+}
